6d2c61bff69d1c6449b275129ea4cecbebbb1ccd
Alexey Fedorov <waterlink000@gmail.com>
2014-02-12 15:16:45 +0200

    cleaned main loop from debug output

--- smiled.rb
+++ smiled.rb
@@ -4,14 +4,12 @@ require 'shellwords'
 
 loop do
   sleep 1
-  r = rand
-  if r < THRESHOLD
+  if rand < THRESHOLD
     time = Shellwords.escape Time.now.to_s
     stamp = Time.now.to_i
-    puts `mkdir -p ~/.smiled/tmp`
-    puts `streamer -f jpeg -o ~/.smiled/tmp/#{stamp}.jpeg`
+    `mkdir -p ~/.smiled/tmp`
+    `streamer -f jpeg -o ~/.smiled/tmp/#{stamp}.jpeg`
     sleep 1
-    puts `mv ~/.smiled/tmp/#{stamp}.jpeg ~/.smiled/#{time}.jpeg`
+    `mv ~/.smiled/tmp/#{stamp}.jpeg ~/.smiled/#{time}.jpeg`
   end
-  puts "r was: #{r}"
 end
